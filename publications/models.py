5b66425a704da0811c3d7ac8ea2500f18f5f4e9e
lucas <lcalandino@gmail.com>
2021-11-02 11:47:47 -0300

    add user logic

--- publications/models.py
+++ publications/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+	DIRETORIA = 'DI'
+	COORDENADOR = 'CO'
+	ASSOCIADO = 'AS'
+	REGISTRADO = 'RE'
+
+	USER_TYPE_CHOICE = [
+		(DIRETORIA, 'Diretoria'),
+		(COORDENADOR, 'Coordenador'),
+		(ASSOCIADO, 'Associado'),
+		(REGISTRADO, 'Registrado')
+	]
+	user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICE, default=REGISTRADO)
